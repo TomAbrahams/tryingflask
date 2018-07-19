2040783285c376e10f3cd12852883be2e6c2dc72
TomAbrahams <utkeitarol@gmail.com>
2018-07-19 15:33:59 -0700

    Time to get to class!

--- routes.py
+++ routes.py
@@ -1,15 +1,16 @@
 #Thank god, we be in python
 from flask import Flask, render_template, request
-from models import db
+from models import db, User
 from forms import SignUpForm
 
 #Got to get that app.
 app = Flask(__name__)
 
 #Connection String
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/learningflask'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///learningflask'
 db.init_app(app) #Initialize app
-#Need to generate forms with secure items.
+#Need to generate forms with secure items. Again this is a demo for learning. otherwise certain items would have been .gitignore d.
+
 app.secret_key = "development-key"
 
 #Setup route for the index
@@ -28,7 +29,11 @@ def signup():
         if form.validate() == False:
             return render_template('signup.html', form=form)
         else:
-            return "Successful Signed up!"
+            newuser = User(form.first_name.data,form.last_name.data, form.email.data, form.password.data)
+            db.session.add(newuser)
+            db.session.commit()
+            return "You've been Successfully Signed up!"
+
     elif request.method =="GET":
         return render_template('signup.html', form=form)
 
