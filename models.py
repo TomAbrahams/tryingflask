813e76398809413747517ba526fe196c5089641d
TomAbrahams <utkeitarol@gmail.com>
2018-07-19 14:45:56 -0700

    Needed the venv... Makes it so much easier...

---
+++ models.py
@@ -0,0 +1,26 @@
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug import generate_password_hash, check_password_hash
+#flask.ext.sqlalchemy is for the connection to the database.
+#werkzeug is for the encryption of the password.
+#db stores a new object of the SQLAlchemy class.
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__= 'users'
+    uid = db.Column(db.Integer, primary_key = True)
+    firstname = db.Column(db.String(109))
+    lastname = db.Column(db.String(109))
+    email = db.Column(db.String(120), unique=True)
+    pwdhash = db.Column(db.String(54))
+    #Initialize, Constructor, Methods
+    def __init__(self, firstname, lastname, email, password):
+        self.firstname = firstname.title()
+        self.lastname = lastname.title()
+        self.email = email.lower()
+        self.set_password(password)
+    #Set Password methods
+    def set_password(self, password):
+        self.pwdhash = generate_password_hash(password)
+    #Check Password methods
+    def check_password(self, password):
+        return check_password_hash(self.pwdhash, password)
--- routes.py
+++ routes.py
@@ -1,13 +1,37 @@
 #Thank god, we be in python
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from models import db
+from forms import SignUpForm
 
+#Got to get that app.
 app = Flask(__name__)
-@app.route("/")
+
+#Connection String
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/learningflask'
+db.init_app(app) #Initialize app
+#Need to generate forms with secure items.
+app.secret_key = "development-key"
+
+#Setup route for the index
+@app.route("/") #Define Route
 def index():
     return render_template("index.html")
+#Setup the route for about
 @app.route("/about")
 def about():
     return render_template("about.html")
+#Setup the route for SignUp
+@app.route("/signup", methods=['GET','POST'])
+def signup():
+    form = SignUpForm()
+    if request.method =="POST":
+        if form.validate() == False:
+            return render_template('signup.html', form=form)
+        else:
+            return "Successful Signed up!"
+    elif request.method =="GET":
+        return render_template('signup.html', form=form)
+
 if __name__ == "__main__":
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.run(debug=True)
